e51891c9f3b43adc2a9855941a459cad81327311
Pedro Ivo <pedroivodefaria@gmail.com>
2020-12-01 16:20:27 -0300

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Pen from './assets/pen.svg'
 
 const ENTER_KEY_CODE = 13
 const DEFAULT_LABEL_PLACEHOLDER = "Click To Edit"
@@ -13,6 +12,7 @@ export default class EditableLabel extends React.Component {
         this.state = {
             isEditing: this.props.isEditing || false,
             text: this.props.text || "",
+            icon: this.props.icon || ""
         }
     }
 
@@ -20,6 +20,7 @@ export default class EditableLabel extends React.Component {
         if (prevProps.text !== this.props.text) {
             this.setState({
                 text: this.props.text || "",
+                icon: this.props.icon || ""
             })
         }
 
@@ -30,11 +31,11 @@ export default class EditableLabel extends React.Component {
         }
     }
 
-    isTextValueValid = () => {
+    isTextValueValid() {
         return (typeof this.state.text != "undefined" && this.state.text.trim().length > 0)
     }
 
-    handleFocus = () => {
+    handleFocus(){
         if (this.state.isEditing) {
             if (typeof this.props.onFocusOut === 'function') {
                 this.props.onFocusOut(this.state.text)
@@ -63,19 +64,19 @@ export default class EditableLabel extends React.Component {
         }
     }
 
-    handleChange = () => {
+    handleChange(){
         this.setState({
             text: this.textInput.value,
         })
     }
 
-    handleKeyDown = (e) => {
+    handleKeyDown(e){
         if (e.keyCode === ENTER_KEY_CODE) {
             this.handleEnterKey()
         }
     }
 
-    handleEnterKey = () => {
+    handleEnterKey(){
         this.handleFocus()
     }
 
@@ -105,6 +106,8 @@ export default class EditableLabel extends React.Component {
         }
 
         const labelText = this.isTextValueValid() ? this.state.text : (this.props.labelPlaceHolder || DEFAULT_LABEL_PLACEHOLDER)
+        const iconLabel = this.state.icon
+
         return <div onClick={this.handleFocus}>
             <label className={this.props.labelClassName}
                 style={{
@@ -113,13 +116,14 @@ export default class EditableLabel extends React.Component {
                 }}>
                 {labelText}
             </label>
-            <Pen/>
+            {iconLabel}
         </div>
     }
 }
 
 EditableLabel.propTypes = {
     text: PropTypes.string.isRequired,
+    icon: PropTypes.any,
     isEditing: PropTypes.bool,
     emptyEdit: PropTypes.bool,
 
